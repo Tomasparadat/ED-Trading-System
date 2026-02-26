5200819f5840017ebdc2a0ae2264773a38f95519
Tomas Parada <3010013@stud.hs-mannheim.de>
2026-02-26 21:24:27 +0100

    Implemented EventProducer.

--- src/main/java/com/trading/sim/EventProducer.java
+++ src/main/java/com/trading/sim/EventProducer.java
@@ -1,19 +1,35 @@
 package com.trading.sim;
 
 import com.lmax.disruptor.RingBuffer;
+import com.trading.domain.EventType;
 import com.trading.infra.event.TradingEvent;
 
 public class EventProducer {
     private RingBuffer<TradingEvent> ringBuffer;
+    private SymbolRegistry symbolRegistry;
 
 
-    public void publishPrice(String symbol, double price) {
-        ringBuffer.p
+    public EventProducer(RingBuffer<TradingEvent> ringBuffer, SymbolRegistry symbolRegistry) {
+        this.ringBuffer = ringBuffer;
+        this.symbolRegistry = symbolRegistry;
     }
 
-    // TODO: Complete implementation.
-    public void publishFill(TradingEvent fill){
-        ringBuffer.publish(fill.getOrderId());
-    }
 
+    /**
+     * Claims next slot in the RingBuffer, clears it, resets EventType and sets new Ticker, price and TimeStamp.
+     *
+     * @param symbol new Ticker given to the TradingEvent.
+     * @param price new Price passed to TradingEvent.
+     */
+    public void publish(String symbol, double price) {
+        int symbolId = symbolRegistry.getSymbolId(symbol);
+        long sequence = ringBuffer.next();
+        try {
+            TradingEvent event = ringBuffer.get(sequence);
+            event.clear();
+            event.set(EventType.MARKET_TICK, symbolId, price, System.currentTimeMillis());
+        } finally {
+            ringBuffer.publish(sequence);
+        }
+    }
 }
